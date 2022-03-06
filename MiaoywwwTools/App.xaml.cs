f14afaa72ffe509c11e2addda50e571f6a776d5a
Miaoywww <375629202@qq.com>
2022-03-06 14:33:45 +0800

    上传项目文件
    上传项目文件

---
+++ MiaoywwwTools/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+
+namespace MiaoywwwTools
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        System.Threading.Mutex mutex;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            bool ret;
+            mutex = new System.Threading.Mutex(true, "AppName", out ret);//这里填写程序名称
+            if (!ret)
+            {
+                Environment.Exit(0);
+            }
+            base.OnStartup(e);
+        }
+    }
+}
