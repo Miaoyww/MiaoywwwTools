79288f0aa458cdc4bbbd383f039f7899e136bd7f
Miaoywww <375629202@qq.com>
2022-03-06 14:35:53 +0800

    上传项目文件
    上传项目文件

---
+++ MiaoywwwTools/EasterEgg.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace MiaoywwwTools
+{
+    /// <summary>
+    /// EasterEgg.xaml 的交互逻辑
+    /// </summary>
+    public partial class EasterEgg : Page
+    {
+        public EasterEgg()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
