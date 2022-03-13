b937d722e49f625457c3d6a6ef68634839a9c4d2
Miaoywww <375629202@qq.com>
2022-03-13 22:17:43 +0800

    ToolsRr更新,适配多种DPI

--- MiaoywwwTools/Tools/RandomDraw/ShowResult.xaml.cs
+++ MiaoywwwTools/Tools/RandomDraw/ShowResult.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using RandomDrawLib;
+using System.Drawing;
 
 namespace MiaoywwwTools.Tools.RandomDraw
 {
@@ -20,6 +21,7 @@ namespace MiaoywwwTools.Tools.RandomDraw
     /// </summary>
     public partial class ShowResult : Window
     {
+        public ShowResult showResult;
         public ShowResult()
         {
             InitializeComponent();
@@ -34,6 +36,24 @@ namespace MiaoywwwTools.Tools.RandomDraw
                 Label_Name.Content = result[0];
                 Label_Grade.Content = result[1];
             }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = System.Windows.WindowState.Normal;//还原窗口（非最小化和最大化）
+            this.WindowStyle = System.Windows.WindowStyle.None; //仅工作区可见，不显示标题栏和边框
+            this.ResizeMode = System.Windows.ResizeMode.NoResize;//不显示最大化和最小化按钮
+            this.Topmost = true;    //窗口在最前
+
+            this.Left = 0.0;
+            this.Top = 0.0;
+            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+        }
+
+        private void Btn_Close_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
